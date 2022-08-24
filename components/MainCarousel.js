6bbe8bb5c75fc3dd16ed1ecb013559277e8ce95e
Swapnil <swapnilshah102002@gmail.com>
2022-08-24 22:31:50 +0530

    removed some bugs

--- components/MainCarousel.js
+++ components/MainCarousel.js
@@ -9,23 +9,23 @@ export default class NextJsCarousel extends Component {
             <div className="carousel-wrapper">
               <Carousel showThumbs = {false} autoPlay = {true} dynamicHeight = {false} infiniteLoop = {true} interval = {2000} >
                   <div>
-                      <img src="https://icon-library.com/images/twitter-small-icon/twitter-small-icon-17.jpg" alt="image1" style = {{ height: '400px' , width: '400px' }}/>
+                      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP._sNqv1NcKw7jGrlSEXcZFAHaHF%26pid%3DApi&f=1" alt="image1" style = {{ height: '400px' , width: '400px' }}/>
                       
                   </div>
                   <div>
-                      <img src="https://sportskreeda.com/wp-content/uploads/2020/12/106398849_580317029261853_4298231535878820781_n-925x1024.jpg" alt="image2" style = {{ height: '400px' , width: '400px' }} />
+                      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.vog5PPffkOEJk14ZrAobJAHaHa%26pid%3DApi&f=1" alt="image2" style = {{ height: '400px' , width: '400px' }} />
                      
                   </div>
                   <div>
-                      <img src="https://moroesports.com/wp-content/uploads/2021/06/channels4_profile-compressed.jpg" alt="image3" style = {{ height: '400px' , width: '400px' }}/>
+                      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.qyOYaWxr5oNMu3G0BpIsLAHaHY%26pid%3DApi&f=1" alt="image3" style = {{ height: '400px' , width: '400px' }}/>
                      
   
                   </div>
                   <div>
-                      <img src="https://adixesports.com/wp-content/uploads/2021/06/20210630_110158_compress93.jpg" alt="image4" style = {{ height: '400px' , width: '400px' }}/>
+                      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.83XpN-IHHM4b_xraWEKJsAHaIq%26pid%3DApi&f=1" alt="image4" style = {{ height: '400px' , width: '400px' }}/>
                   </div>
                   <div>
-                      <img src="https://celebrow.com/wp-content/uploads/2021/07/Payal-Gaming.jpg" alt="image5" style = {{ height: '400px' , width: '400px' }}/>
+                      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.f47bfj7sNyJHkhQOEr88iQHaHa%26pid%3DApi&f=1" alt="image5" style = {{ height: '400px' , width: '400px' }}/>
                   </div>
               </Carousel>
             </div>
